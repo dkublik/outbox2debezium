230896ae41dfad8130cf59797337d5b8cbf2c145
dkublik <dkublik@gmail.com>
2019-05-13 22:04:18 +0200

    first commit

---
+++ src/main/java/pl/dk/outbox2deb/Outbox2debeziumApplication.java
@@ -0,0 +1,13 @@
+package pl.dk.outbox2deb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Outbox2debeziumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Outbox2debeziumApplication.class, args);
+	}
+
+}
